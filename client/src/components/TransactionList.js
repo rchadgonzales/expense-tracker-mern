d4527295cbbc057d45c531922bdbb96fa3da1913
Richard <rchadgonzales@gmail.com>
2022-09-28 16:12:26 +0800

    Initial commit

---
+++ client/src/components/TransactionList.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect } from "react";
+import { Transaction } from "./Transaction";
+
+import { GlobalContext } from "../context/GlobalState";
+
+export const TransactionList = () => {
+  /*
+  const context = useContext(GlobalContext);
+  console.log(context);
+*/
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
+};
